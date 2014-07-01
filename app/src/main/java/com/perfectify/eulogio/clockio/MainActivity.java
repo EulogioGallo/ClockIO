ae5366caef048468f22bc8482737d04bf827a44b
Eulogio Gallo <eulogiogallo90@gmail.com>
2014-07-01 04:17:29 -0700

    Installed apps show up in a list along with icons when applicable

--- app/src/main/java/com/perfectify/eulogio/clockio/MainActivity.java
+++ app/src/main/java/com/perfectify/eulogio/clockio/MainActivity.java
@@ -2,24 +2,76 @@ package com.perfectify.eulogio.clockio;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.perfectify.eulogio.clockio.appList.appList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+
+    // App names and Icons
+    List<String> appNames = new ArrayList<String>();
+    List<Drawable> appIcons = new ArrayList<Drawable>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityManager am = (ActivityManager) this
-                .getSystemService(Activity.ACTIVITY_SERVICE);
-        String packageName = am.getRunningTasks(1).get(0).topActivity
-                .getPackageName();
-        Log.d("CURRENT APP", packageName);
+
+
+        final PackageManager pm = getPackageManager();
+        //get a list of installed packages.
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        //Get listview item from activity_main.xml
+        ListView lv = (ListView) findViewById(R.id.appListView);
+
+
+        //cycle through packages for app names
+        for (ApplicationInfo packageInfo : packages) {
+
+            //Get application name from packagename
+            ApplicationInfo ai;
+            try {
+                ai = pm.getApplicationInfo(packageInfo.packageName, 0);
+            } catch (final PackageManager.NameNotFoundException e) {
+                ai = null;
+            }
+
+            // get app name if available, else get package name
+            final String applicationName = (String) (ai != null ? pm.getApplicationLabel(ai) : packageInfo.packageName);
+            appNames.add(applicationName);
+
+            //find app Icon if available, else get default logo
+            Drawable appIcon;
+            try {
+                appIcon = pm.getApplicationIcon(packageInfo.packageName);
+            } catch (PackageManager.NameNotFoundException nnfe) {
+                appIcon = pm.getDefaultActivityIcon();
+            }
+            appIcons.add(appIcon);
+        }
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        appList adapter = new appList(this, appNames, appIcons);
+
+        lv.setAdapter(adapter);
+
+// the getLaunchIntentForPackage returns an intent that you can use with startActivity()
     }
 
 
---
+++ app/src/main/java/com/perfectify/eulogio/clockio/appList/appList.java
@@ -0,0 +1,41 @@
+package com.perfectify.eulogio.clockio.appList;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.perfectify.eulogio.clockio.R;
+
+import java.util.List;
+
+/**
+ * Custom View that is a list w/icons
+ */
+public class appList extends ArrayAdapter<String> {
+    private final Activity context;
+    private final List<String> name;
+    private final List<Drawable> image;
+    public appList(Activity context, List<String> name, List<Drawable> image) {
+        super(context, R.layout.app_list, name);
+        this.context = context;
+        this.name = name;
+        this.image = image;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.app_list, null, true);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.txt);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+        txtTitle.setText(name.get(position));
+        imageView.setImageDrawable(image.get(position));
+        return rowView;
+    }
+}
